9c8603d93e965ccfdea6797a50641a4162aabdf8
HonzaAntos <honza.ant@email.cz>
2022-08-29 11:12:53 +0200

    if statement card connected

--- attendanceSystem_if_card.py
+++ attendanceSystem_if_card.py
@@ -1,5 +1,5 @@
 ## Toggle an LED when the GUI button is pressed ##
-
+#todo code review - create a classes for every function
 from tkinter import *
 import tkinter.font
 from gpiozero import LED
@@ -87,6 +87,19 @@ new_image_leave = ImageTk.PhotoImage(resized_leave_image)
 leave_label=Label(win, image = new_image_leave)
 leave_label.place(height='120', width='180', relx=0.5, rely=0.2)
 
+#icon doctor
+doctor_image = (Image.open("/home/pi/Downloads/doctor.png"))  ## Add image to rasp!!!!
+resized_doctor_image = doctor_image.resize((100,60), Image.ANTIALIAS)
+new_image_doctor = ImageTk.PhotoImage(resized_doctor_image)
+doctor_label=Label(win, image = new_image_doctor)
+doctor_label.place(height='120', width='180', relx=0.74, rely=0.2)
+
+#icon private session - coffee
+coffee_image = (Image.open("/home/pi/Downloads/coffee.png"))  ## Add image to rasp!!!!
+resized_coffee_image = coffee_image.resize((100,60), Image.ANTIALIAS)
+new_image_coffee = ImageTk.PhotoImage(resized_coffee_image)
+coffee_label=Label(win, image = new_image_coffee)
+coffee_label.place(height='120', width='180', relx=0.02, rely=0.2)
 
 ### REAL TIME ###
 def my_time():
@@ -121,12 +134,23 @@ def ledToggle():
     #    ledButton["text"] = "LED off"
     #    RPi.GPIO.output(buzz, RPi.GPIO.HIGH)
 
+# function to blink red or green when arrival or leave
+def blinkByState(val: bool):
+    if val:
+        RPi.GPIO.output(27, RPi.GPIO.HIGH) #blink red
+        sleep(0.3)
+        RPi.GPIO.output(27, RPi.GPIO.LOW)
+    else:
+        RPi.GPIO.output(28, RPi.GPIO.HIGH) #blink green
+        sleep(0.3)
+        RPi.GPIO.output(28, RPi.GPIO.LOW)
 
 def arrivalToggle():
-    var.set("Příthrthchod")
+    var.set("Příchod")
     global type
     type=1
     bg.set("green")
+    blinkByState(False)
     popup()
    # pop.config(bg='green')
     #label_middleTop["text"]="Příchod"
@@ -137,6 +161,7 @@ def leaveToggle():
     bg.set("red")
     #pop.config(bg='red')
     #label_middleTop["text"]="Odchod"
+    blinkByState(True)
     popup()
 
 def doctorToggle():
@@ -148,56 +173,59 @@ def doctorToggle():
     #label_middleTop["text"]="Lékař"
 
 def popup():
-    global pop
-    global img_arrow
-    pop =Toplevel()
-    pop.geometry("800x420+0+60")
-    print(bg.get())
-    #pop['bg']=bg.get
-    if bg=="blue":
-        pop.configure(bg="blue")
-        print("bg is blue ------")
-    if bg=="red":
-        pop.configure(bg="red")
-        print("bg is red ------")
-    if bg=="green":
-        pop.configure(bg="green")
-        print("bg is green ------")
-    endFont = tkinter.font.Font(family='Helvetica', size=24, weight="bold")
+    if CardValue:
+            global pop
+            global img_arrow
+            pop =Toplevel()
+            pop.geometry("800x420+0+60")
+            print(bg.get())
+            #pop['bg']=bg.get
+            if bg=="blue":
+                pop.configure(bg="blue")
+                print("bg is blue ------")
+            if bg=="red":
+                pop.configure(bg="red")
+                print("bg is red ------")
+            if bg=="green":
+                pop.configure(bg="green")
+                print("bg is green ------")
+            endFont = tkinter.font.Font(family='Helvetica', size=24, weight="bold")
 
-    canvas = Canvas(pop,width=800, height=420)
-    canvas.pack()
-
-
-
-    #i = [1,2,3,4,5,6,7,8,9,10]
-    #for num in i:
-    #    print(num)
-    #    offset = 20*num
-    #    arrow_pic.place(height='400', width='100', x=offset, y='0')
-
-    #img_arrow = (Image.open("/home/pi/Downloads/green_arrow_crop.png"))
-    # Resize image
-    #resized_image_arrow = arrow.resize((100, 300), Image.ANTIALIAS)
-    #new_image_arrow = ImageTk.PhotoImage(resized_image_arrow)
-    # Create a Label Widget to display the text or Image
-    #label_arrow = Label(pop, image=new_image_arrow)
-    #label_arrow.place(height='300', width='100', x='150', y='150')
+            canvas = Canvas(pop,width=800, height=420)
+            canvas.pack()
 
 
-    label_act = Label(pop, bg='#2C708A', textvariable=var, font=endFont)
-    label_act.place(height='80', width='300',relx=0.3, rely=0.15)
-    ##todo get method from API
-    #animation arrow
-    label_user = Label(pop, bg='#2C708A', text='Jan Novák', font=endFont)
-    label_user.place(height='80', width='300',relx=0.3, rely=0.4)
-    label_card = Label(pop, bg='#2C708A', text=CardValue, font=endFont)
-    label_card.place(height='80', width='300',relx=0.3, rely=0.65)
 
-    #pop.attributes('-fullscreen', True)
-    #pop.protocol("WM_DELETE_WINDOW", close)  # cleanup GPIO when user closes window
-    print("popup start")
-    pop.after(3000, popDestroy)
+            #i = [1,2,3,4,5,6,7,8,9,10]
+            #for num in i:
+            #    print(num)
+            #    offset = 20*num
+            #    arrow_pic.place(height='400', width='100', x=offset, y='0')
+
+            #img_arrow = (Image.open("/home/pi/Downloads/green_arrow_crop.png"))
+            # Resize image
+            #resized_image_arrow = arrow.resize((100, 300), Image.ANTIALIAS)
+            #new_image_arrow = ImageTk.PhotoImage(resized_image_arrow)
+            # Create a Label Widget to display the text or Image
+            #label_a rrow = Label(pop, image=new_image_arrow)
+            #label_arrow.place(height='300', width='100', x='150', y='150')
+
+
+            label_act = Label(pop, bg='#2C708A', textvariable=var, font=endFont)
+            label_act.place(height='80', width='300',relx=0.3, rely=0.15)
+            ##todo get method from API
+            #animation arrow
+            label_user = Label(pop, bg='#2C708A', text='Jan Novák', font=endFont)
+            label_user.place(height='80', width='300',relx=0.3, rely=0.4)
+            label_card = Label(pop, bg='#2C708A', text=CardValue, font=endFont)
+            label_card.place(height='80', width='300',relx=0.3, rely=0.65)
+
+            #pop.attributes('-fullscreen', True)
+            #pop.protocol("WM_DELETE_WINDOW", close)  # cleanup GPIO when user closes window
+            print("popup start")
+            #reset caedvalue to zero!!!!
+            CardValue=0
+            pop.after(3000, popDestroy)
 
 def popDestroy():
     pop.destroy()
